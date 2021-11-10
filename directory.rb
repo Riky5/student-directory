38ce500d434c8671de0ea348533136fd58e6737a
riky moroni <riky5.moroni@gmail.com>
2021-11-10 01:48:00 +0000

    Adds another method and completes the student_directory

--- directory.rb
+++ directory.rb
@@ -1,16 +1,52 @@
-@students = []
+# empty array accessible by all methods
+@students = [] 
+
+def interactive_menu
+  loop do
+  # 1.print the menu and ask the user what to do
+    print_menu
+    selection = STDIN.gets.chomp
+   # 3.do what the user has asked
+   process(selection)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      "I don't know what you meant, try again"
+  end
+end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 # get first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
 # while the name is not empty repeat the code
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
 #   get next name from user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -29,42 +65,10 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
-end
-
 def show_students
   print_header
   print
   print_footer
-end
-
-def interactive_menu
-  # 1.print the menu and ask the user what to do
-  loop do
-    print_menu
-  # 2.read the input and save it into a variable
-    selection = gets.chomp
-   # 3.do what the user has asked
-    case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      "I don't know what you meant, try again"
-    end
-  end
 end
 
 def save_students
@@ -79,8 +83,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -88,4 +92,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesnt' exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
